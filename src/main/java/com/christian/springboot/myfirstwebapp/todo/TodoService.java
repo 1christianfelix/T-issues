d4817257d0695f7b13ee0a62df313eb52ffea9b4
Christian Felix <1christianfelix@gmail.com>
2023-07-14 08:49:56 -0700

    Added TodoController and JSP view

--- src/main/java/com/christian/springboot/myfirstwebapp/todo/TodoService.java
+++ src/main/java/com/christian/springboot/myfirstwebapp/todo/TodoService.java
@@ -1,10 +1,14 @@
 package com.christian.springboot.myfirstwebapp.todo;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class TodoService {
-	private static List<Todo> todos;
+	private static List<Todo> todos = new ArrayList();
 	static {
 		todos.add(new Todo(1, "christian", "Learn Spring", 
 				LocalDate.now().plusYears(1), false));
