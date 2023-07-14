1346e508011ad0e07bc31a09538e745008ea0b5f
Christian Felix <1christianfelix@gmail.com>
2023-07-14 07:06:58 -0700

    Created a request Param and modelmap for login controller

--- src/main/java/com/christian/springboot/myfirstwebapp/login/LoginController.java
+++ src/main/java/com/christian/springboot/myfirstwebapp/login/LoginController.java
@@ -1,14 +1,17 @@
 package com.christian.springboot.myfirstwebapp.login;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
-	// /src/main/resources/META-INF/resources/WEB-INF/jsp/sayHello.jsp
+	
 	@RequestMapping("login")
-	public String gotoLogin() {
+	public String gotoLogin(@RequestParam String name, ModelMap model) {
+		model.put("name", name);
 		return "login";
 	}
 
--- src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
@@ -3,6 +3,8 @@
 		<title> Login Page</title>
 	</head>
 	<body>
-		Welcome to the login page!
+		Welcome to the login page ${name}!
+		
+		
 	</body>
 </html>
