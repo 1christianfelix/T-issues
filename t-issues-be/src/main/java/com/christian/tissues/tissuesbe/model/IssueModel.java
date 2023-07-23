e431ff11435f17aecd3bbc09bdf90ffda8f4f044
Christian Felix <1christianfelix@gmail.com>
2023-07-23 01:04:24 -0700

    Issue Model created

---
+++ t-issues-be/src/main/java/com/christian/tissues/tissuesbe/model/IssueModel.java
@@ -0,0 +1,71 @@
+package com.christian.tissues.tissuesbe.model;
+
+import java.time.LocalDate;
+
+public class IssueModel {
+
+	private boolean done;
+	private LocalDate targetDate;
+	private String description;
+	private String username;
+	private int id;
+
+//	Default constructor
+	public IssueModel() {};
+	
+	public IssueModel(int id, String username, String description, LocalDate targetDate, boolean done) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.description = description;
+		this.targetDate = targetDate;
+		this.done = done;
+	}
+
+	public boolean isDone() {
+		return done;
+	}
+
+	public void setDone(boolean done) {
+		this.done = done;
+	}
+
+	public LocalDate getTargetDate() {
+		return targetDate;
+	}
+
+	public void setTargetDate(LocalDate targetDate) {
+		this.targetDate = targetDate;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	@Override
+	public String toString() {
+		return "Todo [id=" + id + ", username=" + username + ", description=" + description + ", targetDate="
+				+ targetDate + ", done=" + done + "]";
+	}
+	
+}
