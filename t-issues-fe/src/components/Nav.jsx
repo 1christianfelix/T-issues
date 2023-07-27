c9a322c94e2d4c070b9dc4a188ad358772f893b5
Christian Felix <1christianfelix@gmail.com>
2023-07-27 16:13:35 -0700

    Login modal component setup

--- t-issues-fe/src/components/Nav.jsx
+++ t-issues-fe/src/components/Nav.jsx
@@ -1,22 +1,22 @@
 import React, { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 
+import LoginModal from "./modals/LoginModal";
+
 const Nav = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-  const toggleMenu = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
-
   return (
-    <nav className="bg-gray-800 p-4">
+    <nav className="bg-primary p-4">
       <div className="container mx-auto">
         <div className="flex items-center justify-between">
-          <div className="text-white font-bold text-xl">Your Logo</div>
+          <div className="text-white font-bold text-xl">T-issues</div>
           <div className="md:flex items-center hidden">
-            <button className="ml-4 px-4 py-2 bg-blue-600 text-white rounded-md">
+            <button
+              className="ml-4 px-4 py-2 bg-accent rounded-md text-accent-content"
+              onClick={() => window.my_modal_2.showModal()}
+            >
               Login
             </button>
+            <LoginModal />
           </div>
           <div className="md:hidden flex items-center">
             <div className="drawer">
@@ -25,7 +25,7 @@ const Nav = () => {
                 {/* Page content here */}
                 <label
                   htmlFor="my-drawer"
-                  className="btn btn-primary drawer-button"
+                  className="btn btn-accent drawer-button"
                 >
                   <GiHamburgerMenu></GiHamburgerMenu>
                 </label>
@@ -35,23 +35,13 @@ const Nav = () => {
                 <ul className="menu p-4 w-80 h-full bg-base-200 text-base-content">
                   {/* Sidebar content here */}
                   <li>
-                    <a>Sidebar Item 1</a>
-                  </li>
-                  <li>
-                    <a>Sidebar Item 2</a>
+                    <a>Login</a>
                   </li>
                 </ul>
               </div>
             </div>
           </div>
         </div>
-        {isMenuOpen && (
-          <div className="md:hidden">
-            <div className="px-2 pt-2 pb-4">
-              <button className="block px-4 py-2 text-white">Login</button>
-            </div>
-          </div>
-        )}
       </div>
     </nav>
   );
