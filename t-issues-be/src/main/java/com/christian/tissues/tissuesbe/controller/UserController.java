b47300f6c9e5195907a8124df49aaf048f464c68
Christian Felix <1christianfelix@gmail.com>
2023-07-27 00:25:13 -0700

    delete user controller created

--- t-issues-be/src/main/java/com/christian/tissues/tissuesbe/controller/UserController.java
+++ t-issues-be/src/main/java/com/christian/tissues/tissuesbe/controller/UserController.java
@@ -1,33 +1,46 @@
 package com.christian.tissues.tissuesbe.controller;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.christian.tissues.tissuesbe.model.User;
-import com.christian.tissues.tissuesbe.repository.UserRepository;
+import com.christian.tissues.tissuesbe.service.UserService;
 
 @RestController
 public class UserController {
 	
-	private UserRepository userRepository;
+	private UserService userService;
 	
-	public UserController(UserRepository userRepository) {
-		this.userRepository = userRepository;
+	public UserController(UserService userService) {
+		this.userService = userService;
 	}
 	
 	@PostMapping("/users")
 	User newUser(@RequestBody User newUser) {
-			return userRepository.save(newUser);
+			return userService.createUser(newUser);
 		}
 	
 	@GetMapping("/users")
 	List<User> getUser() {
-		return userRepository.findAll();
+		return userService.getUsers();
 	}
 	
+	
+	@DeleteMapping("/users/{id}")
+	Optional<User> deleteUser(@PathVariable Long id) {
+		
+		Optional<User> deleted = userService.deleteUser(id);
+		
+		return deleted;
+	}
 
 }
---
+++ t-issues-be/src/main/java/com/christian/tissues/tissuesbe/service/UserService.java
@@ -0,0 +1,41 @@
+package com.christian.tissues.tissuesbe.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.christian.tissues.tissuesbe.model.User;
+import com.christian.tissues.tissuesbe.repository.UserRepository;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+    
+    public User createUser(User newUser) {
+    	return userRepository.save(newUser);
+    }
+    
+    public List<User> getUsers(){
+    	return userRepository.findAll();
+    }
+    
+
+    public User getUser(Long id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    public Optional<User> deleteUser(Long id) {
+    	Optional<User> user = userRepository.findById(id);
+        return user;
+    }
+
+    // Other custom methods can be added if needed.
+}
